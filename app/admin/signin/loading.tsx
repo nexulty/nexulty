58a4be66b33ff430c4d7c6871276a68ac92882d0
strmfelix <email@felixstrohmeier.com>
2025-05-07 21:09:41 +0200

    style: enhance admin signin page UI

--- app/admin/signin/loading.tsx
+++ app/admin/signin/loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from 'lucide-react'
 
 export default function Loading() {
   return (
-    <div className='flex size-full items-center justify-center'>
+    <div className='bg-muted/50 flex size-full items-center justify-center'>
       <Loader className='size-4 animate-spin' />
     </div>
   )
--- app/admin/signin/page.tsx
+++ app/admin/signin/page.tsx
@@ -1,5 +1,6 @@
 import AdminSigninForm from '@/components/forms/admin-signin-form'
 import { getUser } from '@/queries/cached'
+import { Ticket } from 'lucide-react'
 import { redirect } from 'next/navigation'
 
 export default async function Page() {
@@ -10,14 +11,16 @@ export default async function Page() {
   }
 
   return (
-    <div className='flex size-full items-center justify-center'>
-      <div className='rounded-lg border p-8 shadow-lg'>
+    <div className='bg-muted/50 flex size-full items-center justify-center'>
+      <div className='bg-background rounded-lg border p-8 shadow-lg'>
         <div className='flex items-center gap-2'>
-          <div className='bg-primary size-8 rounded-md' />
-          <h2 className='text-2xl font-semibold tracking-tight'>Admin</h2>
+          <div className='border-primary from-primary shadow-[inset_0_1px_0_0_theme(colors.primary-foreground/15%)] flex size-7 items-center justify-center rounded-md border bg-gradient-to-t to-blue-500'>
+            <Ticket className='size-3.5 text-white' />
+          </div>
+          <h2 className='text-lg font-semibold tracking-tight'>Admin</h2>
         </div>
         <div className='mt-4 mb-8'>
-          <h1 className='text-lg font-semibold tracking-tight'>
+          <h1 className='text-xl font-semibold tracking-tight'>
             Welcome back!
           </h1>
           <p className='text-muted-foreground text-sm'>
